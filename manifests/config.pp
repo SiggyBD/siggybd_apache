79da40dee7c66301351a8fb2e5c99a7dc052ba17
root <root@ip-172-31-39-47.eu-west-1.compute.internal>
2017-01-17 09:19:46 -0500

    bumped version

--- manifests/config.pp
+++
@@ -1,9 +0,0 @@
- class apache::install inherits apache {
-
-   file { '/var/www/html/index.html':
-     ensure => file,
-     node   => '0644',
-     content => template('apache/index.html.erb'),
-   }
-
- }
